5d80be3c7e8471f8473ee6599cdd7b55d4c6e9fd
rushilbr225 <99274426+rushilbr225@users.noreply.github.com>
2024-07-02 08:28:36 +0000

    dynamic pages to each job added

--- database.py
+++ database.py
@@ -19,6 +19,21 @@ def load_jobs_from_db():
       result_dicts.append(dict(row._mapping))
     return result_dicts
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result=conn.execute(
+      text(f"select * from jobs where id ={id}")
+    )
+    rows=[]
+    for row in result.all():
+      rows.append(row._mapping)
+    if len(rows)==0:
+      return None
+    else:
+      return row
+    
+
+  
 
   
 
